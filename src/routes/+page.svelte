dd6b1ae3b4a01598cb0495af7058c98753ba5d15
Ah3ron <ah3ron@protonmail.com>
2025-04-26 22:33:12 +0300

    Init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+db:<script>
+	import { enhance } from '$app/forms';
+
+	let { data } = $props();
+</script>
+
+<h1>Hi, {data.user.username}!</h1>
+<p>Your user ID is {data.user.id}.</p>
+<form method="post" action="?/logout" use:enhance>
+	<button>Sign out</button>
+</form>
